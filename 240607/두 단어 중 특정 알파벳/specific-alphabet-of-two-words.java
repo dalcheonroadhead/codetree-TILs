6a6503f2923285771d130a812a60d1606715b127
Jeon Soo Min <102154788+dalcheonroadhead@users.noreply.github.com>
2024-06-07 23:40:15 +0900

    Title: 두 단어 중 특정 알파벳, Time: 87ms, Memory: 8MB, Status: Wrong Answer - CodeTree

--- 240607/두 단어 중 특정 알파벳/specific-alphabet-of-two-words.java
+++ 240607/두 단어 중 특정 알파벳/specific-alphabet-of-two-words.java
@@ -1,5 +1,44 @@
+import java.io.*;
+import java.util.*;
+
 public class Main {
-    public static void main(String[] args) {
-        // 여기에 코드를 작성해주세요.
+
+    static int n;
+    static int [] arr = new int [26];
+    static boolean [] isVisited; 
+
+    public static void main(String[] args) throws IOException {
+        
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        n = Integer.parseInt(br.readLine());
+
+        for(int i = 0; i < n; i ++) {
+
+            isVisited = new boolean[26];
+            StringTokenizer st  = new StringTokenizer(br.readLine());
+            String s1 = st.nextToken();
+            String s2 = st.nextToken();
+
+            for(int j = 0; j < s1.length(); j++){
+                int temp = s1.charAt(j);
+                if(!isVisited[temp-97]){
+                    arr[temp-97]++;
+                    isVisited[temp-97] = true;
+                }
+            }
+
+            for(int j = 0; j< s2.length(); j++){
+                int temp = s2.charAt(j);
+                if(!isVisited[temp-97]){
+                    arr[temp-97]++;
+                    isVisited[temp-97] = true;
+                }
+            }
+        }
+
+        for(int i = 0; i < arr.length; i++){
+            System.out.println(arr[i]);
+        }
+
     }
 }
