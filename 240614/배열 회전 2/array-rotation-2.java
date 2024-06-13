61a58663905cebffb0a6e70fbb1f9e279b82ca27
Jeon Soo Min <102154788+dalcheonroadhead@users.noreply.github.com>
2024-06-14 02:27:57 +0900

    Title: 배열 회전 2, Time: 1172ms, Memory: 34MB, Status: Passed - CodeTree

--- 240614/배열 회전 2/array-rotation-2.java
+++ 240614/배열 회전 2/array-rotation-2.java
@@ -8,6 +8,9 @@ public class Main {
     static int [] idx = new int []{1,0,-1,0};
     static int [] idy = new int []{0,1,0,-1};
 
+    static int [] revIdx = new int []{0,1,0,-1};
+    static int [] revIdy = new int []{1,0,-1,0};
+
     
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -24,12 +27,26 @@ public class Main {
             }
         }
 
-        // 2. 중앙 확인 : 중앙에서 가로 갈수록, jump 하는 수가 늘어나야 한다.
-        int jump = 1; 
-        for(int i = (n/2)-1; i >= 0; i--){
-            rotate(i,i,jump++);
+
+        // 2. 중앙 확인 및 회전  중앙에서 가로 갈수록, jump 하는 수가 늘어나야 한다.
+        if( d >= 0) {
+           for(int k =0; k <(d/45); k++){
+                int jump = 1; 
+                for(int i = (n/2)-1; i >= 0; i--){
+                    rotate(i,i,jump++);
+                }
+           }     
+        }else {
+             for(int k =0; k <(Math.abs(d)/45); k++){
+                int jump = 1; 
+                for(int i = (n/2)-1; i >= 0; i--){
+                    rev_rotate(i,i,jump++);
+                }
+           }   
         }
 
+
+        // 3. 출력
         for(int [] temp : matrix){
            for(int a : temp){
                 System.out.print(a + " ");
@@ -65,5 +82,32 @@ public class Main {
         }
 
         matrix[x][y+jump] = temp;
+    }
+
+        public static void rev_rotate(int x, int y, int jump) {
+        
+        int temp = matrix[x][y];
+
+        int prevX = x;
+        int prevY = y;
+
+        for(int i = 0; i < 4; i++){
+            // 초기값
+            int nx = prevX;
+            int ny = prevY;
+
+            for(int j = 0; j < 2; j++) {
+                // 초기값에서 해당 방향으로 점프
+                nx += revIdx[i] * jump;
+                ny += revIdy[i] * jump; 
+                // 이동한 값을 이전 위치에 집어넣기
+                matrix[prevX][prevY] = matrix[nx][ny];
+                // 이전 위치 없데이트 
+                prevX = nx;
+                prevY = ny;
+            }
+        }
+
+        matrix[x+jump][y] = temp;
     }
 }
